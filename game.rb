9d8c6b85acc75e3d5a6a893ece5dc062094f0d69
Jefferson-Faseler <jjfaseler@gmail.com>
2017-03-06 13:01:57 -0500

    Game changes turn.

--- game.rb
+++ game.rb
@@ -4,8 +4,16 @@ class Game
     @turn = 'red'
   end
 
+  def change_turn
+    if @turn == 'red'
+      @turn = 'black'
+    elsif @turn == 'black'
+      @turn = 'red'
+    end
+  end
+
   def turn
     @turn
   end
-  
+
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -3,7 +3,15 @@ require_relative "../game"
 describe Game do
   let(:game) { Game.new }
 
-  it "has a turn" do
-    expect(game.turn).to eq('red')
+  describe "game turn" do
+    it "is readable" do
+      expect(game.turn).to eq('red')
+    end
+
+    it "changes to other player" do
+      game.change_turn
+      expect(game.turn).to eq('black')
+    end
   end
+
 end
